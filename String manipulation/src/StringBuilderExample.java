f73c141ffa6da106f8e0558625020acf4a2b881c
ashish kamble <ashish.kamble@infostretch.com>
2021-12-22 20:54:53 +0530

    java programs

--- String manipulation/src/StringBuilderExample.java
+++
@@ -1,7 +0,0 @@
-class StringBuilderExample{  
-public static void main(String args[]){  
-StringBuilder sb=new StringBuilder("Hello ");  
-sb.append("Java");  
-System.out.println(sb);
-}  
-}
--- String manipulation/src/TestImmutablestring.java
+++
@@ -1,7 +0,0 @@
-class Testimmutablestring{  
- public static void main(String args[]){  
-   String s="Sachin";  
-  s= s.concat(" Tendulkar"); 
-   System.out.println(s);  
- }  
-}
