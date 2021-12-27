701790362737a61602d457afb88f8f6af3c7714d
ashish kamble <ashish.kamble@infostretch.com>
2021-12-27 10:53:39 +0530

    java tasks

---
+++ Basic string operations/src/StrTrim.java
@@ -0,0 +1,10 @@
+public class StrTrim {
+	  public static void main(String[] args) {    
+	        String s1 ="  I am a girl   ";    
+	        System.out.println(s1.length());    
+	        System.out.println(s1);     
+	        String tr = s1.trim();    
+	        System.out.println(tr.length());    
+	        System.out.println(tr);     
+	    }    
+}
