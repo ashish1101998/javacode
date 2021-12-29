1d93570f8ca48ddea45c944470a00784c2c6d68c
ashish kamble <ashish.kamble@infostretch.com>
2021-12-29 12:54:42 +0530

    First add file

---
+++ Basic string operations/src/StrCompare.java
@@ -0,0 +1,12 @@
+public class StrCompare {
+
+		   public static void main(String args[]){  
+		   String s1="String Compare";  
+		   String s2="String Compare";  
+		   String s3=new String("String Compare");  
+		   String s4="String Concat";  
+		   System.out.println(s1.equals(s2));
+		   System.out.println(s1.equals(s3));  
+		   System.out.println(s1.equals(s4)); 
+		 }  
+		}
---
+++ Basic string operations/src/StrLength.java
@@ -0,0 +1,13 @@
+public class StrLength {
+
+	    public static void main(String[] args) {  
+	        String str = "Infostretch";  
+	        if(str.length()>0) {  
+	            System.out.println("First string length is: "+str.length());  
+	        }  
+	        str = "Company";  
+	        if(str.length()>0) {  
+	            System.out.println("Second string length is: "+str.length());  
+	        }  
+	    }  
+	}
